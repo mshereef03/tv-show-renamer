3242c2bc2863e17f89f456619dfb9511153e6907
mshereef03 <mshereef2003@gmail.com>
2023-03-18 22:39:57 +0200

    Added option to rename subs that are nested in folders.

--- src/EpisodeRenamer.java
+++ src/EpisodeRenamer.java
@@ -1,37 +1,42 @@
+import javax.swing.*;
 import java.io.File;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.Arrays;
 import java.util.Scanner;
+
+// basic and complex subs break if more than 26 episodes due to lexicrophaical sorting of files
+// didnt test basic subs but gg
 
 
 public class EpisodeRenamer {
 
     final static String apiKey = "16edf408";
 
-    public static void renameEpisodes(String folderPath,String extension,String showName,String seasonNum) throws InterruptedException {
+    public static void renameEpisodes(String folderPath, String extension, String showName, String seasonNum, JTextArea outputArea, boolean isBasicSubs, boolean isComplexSubs) throws InterruptedException {
 
-        //Requirements: Have all episodes in the folder and specify the 4 parameters below.
-
-//         folderPath = "/Users/mshereef/Desktop/South.Park.S14.1080p.BluRay.x264-UNTOUCHABLES[rartv]"; // Update this to the path of your folder
-//         extension = ".mkv"; // Type of file being changed
-//         showName = "South Park";// Update this to the name of your TV show
-//         seasonNum = "14"; // Season of show
 
         File folder = new File(folderPath);
+        if(isBasicSubs)mergeFoldersOneSubtitle(folder);
+        if(isComplexSubs)mergeFoldersMultipleLangauges(folder);
         folder = new File(folderPath);
         File[] files = folder.listFiles();
         Arrays.sort(files);
+        System.out.println(Arrays.toString(files));
 
-        int counter = 1;
+        int episodeCounter = 1;
 
         for (File file : files) {
             if (file.isFile() && file.getName().endsWith(extension)) {
                 String fileName = file.getName();
 
                 try {
-                    URL url = new URL("http://www.omdbapi.com/?apikey=" + apiKey + "&t=" + showName + "&season=" + seasonNum + "&episode=" + counter);
+                    URL url = new URL("http://www.omdbapi.com/?apikey=" + apiKey + "&t=" + showName + "&season=" + seasonNum + "&episode=" + episodeCounter);
                     HttpURLConnection conn = (HttpURLConnection) url.openConnection();
                     conn.setRequestMethod("GET");
                     conn.connect();
@@ -44,22 +49,101 @@ public class EpisodeRenamer {
                         scanner.close();
 
                         String episodeName = response.substring(response.indexOf("Title\":\"") + 8, response.indexOf("\",\"Year\""));
-                        String newFileName = "S"+seasonNum+"E"+(counter<10?"0"+counter:counter) + " - " + episodeName + extension;
+                        String newFileName = showName+" - S"+seasonNum+"E"+(episodeCounter<10?"0"+episodeCounter:episodeCounter) + " - " + episodeName + extension;
                         File newFile = new File(folderPath +"/"+ newFileName);
 
                         if (file.renameTo(newFile)) {
                             System.out.println(fileName + " renamed to " + newFileName);
                         } else {
                             System.out.println("Failed to rename " + fileName);
+                            break;
                         }
                     } else {
                         System.out.println("Failed to get episode name for " + fileName);
+                        break;
                     }
-                    counter++;
+                    outputArea.update(outputArea.getGraphics());
+                    episodeCounter++;
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
+
+
         }
     }
+
+    public static void mergeFoldersOneSubtitle(File folder) {
+        if (!folder.isDirectory()) {
+            return;
+        }
+        int c = 'a';
+        File[] subfolders = folder.listFiles(File::isDirectory);
+        Arrays.sort(subfolders);
+        for (File subfolder : subfolders) {
+            File[] files = subfolder.listFiles();
+            for (File file : files) {
+                String fileName = file.getName();
+                String extension = fileName.substring(fileName.lastIndexOf('.'));
+                File destFile = new File(folder, "English_"+(char)c+extension);
+
+                try {
+                    Files.move(file.toPath(), destFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                c++;
+            }
+            // Delete the subfolder after moving its files
+            subfolder.delete();
+        }
+    }
+
+    public static void mergeFoldersMultipleLangauges(File folder) {
+        if (!folder.isDirectory()) {
+            return;
+        }
+        int c = 'a';
+        File[] subfolders = folder.listFiles(File::isDirectory);
+        Arrays.sort(subfolders);
+        for (File subfolder : subfolders) {
+            File[] files = subfolder.listFiles();
+            for (File file : files) {
+                String fileName = file.getName();
+                if (fileName.contains("English")) {
+                    String extension = fileName.substring(fileName.lastIndexOf('.'));
+                    File destFile = new File(folder, "English_"+(char)c+extension);
+                    try {
+                        Files.move(file.toPath(), destFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    c++;
+                    break;
+                } else {
+                    file.delete();
+                }
+            }
+
+            deleteDirectory(subfolder);
+
+        }
+    }
+
+    private static void deleteDirectory(File directory) {
+        if (!directory.isDirectory()) {
+            return;
+        }
+        File[] files = directory.listFiles();
+        for (File file : files) {
+            if (file.isDirectory()) {
+                deleteDirectory(file);
+            } else {
+                file.delete();
+            }
+        }
+        directory.delete();
+    }
+
 }
+
--- src/EpisodeRenamerGUI.java
+++ src/EpisodeRenamerGUI.java
@@ -1,5 +1,7 @@
+import com.apple.eawt.Application;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
-import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -7,8 +9,11 @@ import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
 
-public class EpisodeRenamerGUI extends JFrame {
+public class EpisodeRenamerGUI extends JFrame implements ActionListener {
 
     private final JLabel folderLabel;
     private final JTextField folderField;
@@ -25,9 +30,19 @@ public class EpisodeRenamerGUI extends JFrame {
 
     private final JButton runButton;
 
+    private final JCheckBox basic;
+    private final JCheckBox complex;
+
+
+
+    private JTextArea outputArea;
+
+    private JPanel panel;
+
+    final static String apiKey = "16edf408";
+
     public EpisodeRenamerGUI() {
         super("Episode Renamer");
-
         folderLabel = new JLabel("Folder:");
         folderField = new JTextField(20);
         folderField.setEditable(true);
@@ -46,32 +61,19 @@ public class EpisodeRenamerGUI extends JFrame {
             }
         });
 
-        extensionLabel = new JLabel("Extension:");
+        extensionLabel = new JLabel("Extension: (.srt)");
         extensionField = new JTextField(10);
 
-        showNameLabel = new JLabel("Show Name:");
+        showNameLabel = new JLabel("Show Name: (South Park)");
         showNameField = new JTextField(20);
 
-        seasonNumLabel = new JLabel("Season Number:");
+        seasonNumLabel = new JLabel("Season Number: (05)");
         seasonNumField = new JTextField(10);
 
         runButton = new JButton("Run");
-        runButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String folderPath = folderField.getText();
-                String extension = extensionField.getText();
-                String showName = showNameField.getText();
-                String seasonNum = seasonNumField.getText();
-                try {
-                    EpisodeRenamer.renameEpisodes(folderPath, extension, showName, seasonNum);
-                } catch (InterruptedException ex) {
-                    ex.printStackTrace();
-                }
-            }
-        });
 
-        JPanel panel = new JPanel(new GridBagLayout());
+
+        panel = new JPanel(new GridBagLayout());
         GridBagConstraints constraints = new GridBagConstraints();
         constraints.gridx = 0;
         constraints.gridy = 0;
@@ -105,7 +107,7 @@ public class EpisodeRenamerGUI extends JFrame {
         panel.add(runButton, constraints);
 
         // Create a new JTextArea component to display the output
-        JTextArea outputArea = new JTextArea(10, 40);
+        outputArea = new JTextArea(10, 40);
         outputArea.setEditable(false);
 
         // Create a new PrintStream that redirects the standard output stream to the JTextArea
@@ -122,36 +124,73 @@ public class EpisodeRenamerGUI extends JFrame {
         constraints.fill = GridBagConstraints.BOTH;
         panel.add(new JScrollPane(outputArea), constraints);
 
-        setContentPane(panel);
+        JPanel bottom = new JPanel(new GridLayout());
+
+        basic = new JCheckBox("Nested Subs");
+        complex = new JCheckBox("Nested Subs With Multiple Languages");
+        bottom.add(basic);
+        bottom.add(complex);
+
+        add(panel,BorderLayout.CENTER);
+        add(bottom,BorderLayout.SOUTH);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         pack();
         setLocationRelativeTo(null);
+
+
     }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                EpisodeRenamerGUI gui = new EpisodeRenamerGUI();
-                gui.setVisible(true);
-                gui.runButton.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        String folderPath = gui.folderField.getText();
-                        String extension = gui.extensionField.getText();
-                        String showName = gui.showNameField.getText();
-                        String seasonNum = gui.seasonNumField.getText();
-                        try {
-                            EpisodeRenamer.renameEpisodes(folderPath, extension, showName, seasonNum);
-                            JOptionPane.showMessageDialog(new EpisodeRenamerGUI(), "Renaming Completed !");
-                        } catch (InterruptedException ex) {
-                            ex.printStackTrace();
-                        }
-                    }
-                });
-            }
-        });
+
+        EpisodeRenamerGUI gui = new EpisodeRenamerGUI();
+        gui.setVisible(true);
+        gui.runButton.addActionListener(gui);
+
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String folderPath = folderField.getText();
+        String extension = extensionField.getText();
+        String showName = showNameField.getText();
+        String seasonNum = seasonNumField.getText();
+        try {
+            URL url = new URL("http://www.omdbapi.com/?apikey=" + apiKey + "&t=" + showName + "&season=" + seasonNum);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+
+            int responseCode = conn.getResponseCode();
+
+            Scanner scanner = new Scanner(url.openStream());
+            String response = scanner.useDelimiter("\\Z").next();
+            scanner.close();
+
+            char status = response.substring(response.indexOf("Response\":\"") + 11).charAt(0);
+
+            if(extension.startsWith(".")&&responseCode==200&&status=='T'&&!(basic.isSelected()&&complex.isSelected())) {
+
+                repaint();
+                System.out.println("Starting...");
+                outputArea.update(outputArea.getGraphics());
+                EpisodeRenamer.renameEpisodes(folderPath, extension, showName, seasonNum,outputArea,basic.isSelected(),complex.isSelected());
+                JOptionPane.showMessageDialog(new EpisodeRenamerGUI(), "Renaming Completed !");
+            }
+            else{
+                repaint();
+                System.out.println("Failure.");
+                outputArea.update(outputArea.getGraphics());
+                if(!extension.startsWith("."))JOptionPane.showMessageDialog(new EpisodeRenamerGUI(), "Invalid Extension, e.g. '.mkv'");
+                else if(basic.isSelected()&&complex.isSelected()) JOptionPane.showMessageDialog(new EpisodeRenamerGUI(), "Can't select both types of subs !");
+                else JOptionPane.showMessageDialog(new EpisodeRenamerGUI(), "Couldn't find show or API Failure");
+            }
+        } catch (InterruptedException |IOException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
+
 
     private static class CustomOutputStream extends OutputStream {
         private final JTextArea textArea;
@@ -167,6 +206,29 @@ public class EpisodeRenamerGUI extends JFrame {
             // Scroll to the bottom of the JTextArea
             textArea.setCaretPosition(textArea.getDocument().getLength());
         }
+    }
+
+    private class HintTextField extends JTextField {
+        public HintTextField(String hint) {
+            _hint = hint;
+        }
+        @Override
+        public void paint(Graphics g) {
+            super.paint(g);
+            if (getText().length() == 0) {
+                int h = getHeight();
+                ((Graphics2D)g).setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+                Insets ins = getInsets();
+                FontMetrics fm = g.getFontMetrics();
+                int c0 = getBackground().getRGB();
+                int c1 = getForeground().getRGB();
+                int m = 0xfefefefe;
+                int c2 = ((c0 & m) >>> 1) + ((c1 & m) >>> 1);
+                g.setColor(new Color(c2, true));
+                g.drawString(_hint, ins.left, h / 2 + fm.getAscent() / 2 - 2);
+            }
+        }
+        private final String _hint;
     }
 
 }
